5585204e9fbe0fe6a772059f0874cb26d57db525
john <john@onyx-international.com>
2011-05-20 22:52:13 +0800

    clean code.

--- code/src/ui/brightness_dialog.cpp
+++ code/src/ui/brightness_dialog.cpp
@@ -5,6 +5,8 @@
 
 namespace ui
 {
+static const int MY_WIDTH = 20;
+static const int MY_HEIGHT = 150;
 
 static const QString FANCY_STYLE = "                                \
     QSlider::groove:horizontal                                      \
@@ -13,16 +15,16 @@ static const QString FANCY_STYLE = "                                \
         background: qlineargradient(x1: 0, y1: 0,    x2: 0, y2: 1,  \
                                     stop: 0 #6666e, stop: 1 #b66bf);\
         background: qlineargradient(x1: 0, y1: 0.2, x2: 1, y2: 1,   \
-                                    stop: 0 #b23bf, stop: 1 #3455f);    \
+                                    stop: 0 #b23bf, stop: 1 #3455f);\
         height: 40px;                                               \
         border-radius: 8px;                                         \
     }                                                               \
     QSlider::sub-page:horizontal                                    \
     {                                                               \
         background: qlineargradient(x1: 0, y1: 0,    x2: 0, y2: 1,  \
-                                    stop: 0 #66e, stop: 1 #bbf);    \
+                                    stop: 0 #e366e, stop: 1 #bbbbf);\
         background: qlineargradient(x1: 0, y1: 0.2, x2: 1, y2: 1,   \
-                                    stop: 0 #bbf, stop: 1 #55f);    \
+                                    stop: 0 #bbbbf, stop: 1 #dd55f);\
         border: 1px solid #777;                                     \
         height: 40px;                                               \
         border-radius: 4px;                                         \
@@ -91,14 +93,17 @@ BrightnessDialog::~BrightnessDialog(void)
 
 int BrightnessDialog::exec()
 {
-    QWidget *p = ui::safeParentWidget(parentWidget());
-    setGeometry(20, p->height() - 150, p->width() - 40, 150);
+    QWidget *p = ui::safeParentWidget(0);
+    setFixedWidth(p->width());
+    setFixedHeight(MY_HEIGHT);
+    show();
+    move(p->frameGeometry().left(), p->frameGeometry().bottom() - MY_HEIGHT);
     return QDialog::exec();
 }
 
 void BrightnessDialog::createLayout()
 {
-    layout_.setContentsMargins(0, 2, 0, 2);
+    layout_.setContentsMargins(MY_WIDTH, 2, MY_WIDTH, 2);
 
     title_.useTitleBarStyle();
     title_.setAlignment(Qt::AlignCenter);
@@ -110,6 +115,7 @@ void BrightnessDialog::createLayout()
     slider_.setStyleSheet(FANCY_STYLE);
     slider_.setValue(sys::SysStatus::instance().brightness());
     layout_.addWidget(&slider_);
+    layout_.addSpacing(40);
 
     connect(&slider_, SIGNAL(valueChanged(int)), this, SLOT(onValueChanged(int)));
 }
