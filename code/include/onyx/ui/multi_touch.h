0676c8579d88a196d8f0f22e7ed1c75174ec1059
john <john@onyx-international.com>
2011-11-13 17:36:18 +0800

    added multi touch helper class. https://trakrapp.com/trakr/a#projects/boox/issues/880.

---
+++ code/include/onyx/ui/multi_touch.h
@@ -0,0 +1,34 @@
+#ifndef MULTI_TOUCH_H_
+#define MULTI_TOUCH_H_
+
+#include <QtGui/QtGui>
+#include "onyx/base/base.h"
+
+
+/// Class for multi touch. Caller can use this class to
+/// calculate the new zooming factor and new image position.
+/// This class generates a low quality image during multi touch holding.
+class MultiTouch
+{
+public:
+    MultiTouch();
+    ~MultiTouch();
+
+public:
+    void onMultiTouchHoldDetected(QWidget *wnd, QRect r1, QRect r2, int prev, int now);
+    void onMultiTouchReleaseDetected(QRect r1, QRect r2);
+
+    QPixmap * pixmap();
+    QPoint position();
+
+    int diagonal(const QRect & rc);
+
+private:
+    scoped_ptr<QPixmap> pixmap_;
+    QRect rc_touched_;
+    qreal zoom_;
+    bool dirty_;
+};
+
+#endif
+
---
+++ code/src/ui/multi_touch.cpp
@@ -0,0 +1,51 @@
+
+#include "onyx/ui/multi_touch.h"
+
+MultiTouch::MultiTouch()
+{
+}
+
+MultiTouch::~MultiTouch()
+{
+}
+
+void MultiTouch::onMultiTouchHoldDetected(QWidget *wnd, QRect r1, QRect r2, int prev, int now)
+{
+    dirty_ = true;
+
+    // Just touched.
+    if (prev == 0)
+    {
+        pixmap_.reset(new QPixmap(wnd->size()));
+        wnd->render(pixmap_.get());
+        rc_touched_.setCoords(r1.center().x(), r1.center().y(), r2.center().x(), r2.center().y());
+        zoom_ = 1.0;
+    }
+    else
+    {
+        zoom_ = static_cast<qreal>(now) / static_cast<qreal>(prev);
+    }
+}
+
+int MultiTouch::diagonal(const QRect & rc)
+{
+    int l = rc.width() * rc.width() + rc.height() * rc.height();
+    return static_cast<int>(sqrt(static_cast<qreal>(l)));
+}
+
+void MultiTouch::onMultiTouchReleaseDetected(QRect r1, QRect r2)
+{
+    pixmap_.reset(0);
+}
+
+QPixmap * MultiTouch::pixmap()
+{
+    if (dirty_ && pixmap_)
+    {
+        *pixmap_ = pixmap_->scaled(pixmap_->width() * zoom_, pixmap_->height() * zoom_);
+        dirty_ = false;
+    }
+    return pixmap_.get();
+}
+
+
