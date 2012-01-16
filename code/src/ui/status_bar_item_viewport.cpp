505ec113e8e98a893006f18a17c2836b929a4a6f
john <john@onyx-international.com>
2012-01-16 22:46:05 +0800

    improve display a little bit.

--- code/src/ui/status_bar_item_viewport.cpp
+++ code/src/ui/status_bar_item_viewport.cpp
@@ -6,7 +6,7 @@ namespace ui
 StatusBarItemViewport::StatusBarItemViewport(QWidget *parent)
     : StatusBarItem(VIEWPORT, parent)
 {
-    setFixedWidth(28);
+    setFixedWidth(30);
 }
 
 StatusBarItemViewport::~StatusBarItemViewport(void)
@@ -18,16 +18,31 @@ void StatusBarItemViewport::setViewport(const QRect & parent,
 {
     parent_ = parent;
     child_ = child;
+    if (child_.width() > parent_.width() || child_.height() > parent_.height())
+    {
+        QRect tmp = parent_;
+        parent_ = child_;
+        child_ = tmp;
+    }
+
+    // scale and switch.
+    const int WIDTH = width() - 2;
+    double scale = parent_.width() / WIDTH;
+    QRect bk(0, 0, WIDTH, static_cast<int>(parent_.height() / scale));
+    QRect fg(child_.left() / scale, child_.top() / scale, child_.width() / scale, child_.height() / scale);
+
+    parent_ = bk;
+    child_ = fg;
     update();
 }
 
 void StatusBarItemViewport::paintEvent(QPaintEvent *pe)
 {
     QPainter painter(this);
-    painter.fillRect(parent_, QBrush(QColor(0, 0, 0)));
+    painter.fillRect(parent_.adjusted(1, 1, -1, -1), QBrush(QColor(100, 100, 100)));
     painter.setPen(Qt::white);
     painter.drawRect(parent_);
-    painter.fillRect(child_, QBrush(QColor(255, 255, 255)));
+    painter.fillRect(child_.adjusted(1, 1, -1, -1), QBrush(QColor(255, 255, 255)));
 }
 
 }
