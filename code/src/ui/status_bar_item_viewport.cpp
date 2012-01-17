3526365a01b40e4b8c88cda4b813b1f42628100a
john <john@onyx-international.com>
2012-01-17 12:05:32 +0800

    improved viewport display.

--- code/src/ui/status_bar_item_viewport.cpp
+++ code/src/ui/status_bar_item_viewport.cpp
@@ -3,6 +3,8 @@
 namespace ui
 {
 
+static QRect s_bounding;
+
 StatusBarItemViewport::StatusBarItemViewport(QWidget *parent)
     : StatusBarItem(VIEWPORT, parent)
 {
@@ -18,31 +20,37 @@ void StatusBarItemViewport::setViewport(const QRect & parent,
 {
     parent_ = parent;
     child_ = child;
-    if (child_.width() > parent_.width() || child_.height() > parent_.height())
-    {
-        QRect tmp = parent_;
-        parent_ = child_;
-        child_ = tmp;
-    }
+    s_bounding = parent | child;
 
-    // scale and switch.
-    const int WIDTH = width() - 2;
-    double scale = parent_.width() / WIDTH;
-    QRect bk(0, 0, WIDTH, static_cast<int>(parent_.height() / scale));
-    QRect fg(child_.left() / scale, child_.top() / scale, child_.width() / scale, child_.height() / scale);
+    const int w = static_cast<double>(width() - 2);
+    const int h = static_cast<double>(height() - 2);
+    double scale = std::min(w / static_cast<double>(s_bounding.width()),
+                            h / static_cast<double>(s_bounding.height()));
 
-    parent_ = bk;
-    child_ = fg;
+    int left = (w - s_bounding.width() * scale) / 2 + 1;
+    int top = (h - s_bounding.height() * scale) / 2 + 1;
+
+    QRect r = QRect(left, top, s_bounding.width() * scale, s_bounding.height() * scale);
+    parent_ = QRect(left + (parent_.left() - s_bounding.left()) * scale,
+                    top + (parent_.top() - s_bounding.top()) * scale,
+                    parent_.width() * scale,
+                    parent_.height() * scale);
+    child_ = QRect(left + (child_.left() - s_bounding.left()) * scale,
+                    top + (child_.top() - s_bounding.top()) * scale,
+                    child_.width() * scale,
+                    child_.height() * scale);
+    s_bounding = r;
     update();
 }
 
 void StatusBarItemViewport::paintEvent(QPaintEvent *pe)
 {
     QPainter painter(this);
-    painter.fillRect(parent_.adjusted(1, 1, -1, -1), QBrush(QColor(100, 100, 100)));
+    painter.fillRect(parent_, QBrush(QColor(100, 100, 100, 200)));
+    painter.fillRect(child_, QBrush(QColor(255, 255, 255, 200)));
     painter.setPen(Qt::white);
-    painter.drawRect(parent_);
-    painter.fillRect(child_.adjusted(1, 1, -1, -1), QBrush(QColor(255, 255, 255)));
+    painter.drawRect(s_bounding.adjusted(-1, -1, 0, -1));
+
 }
 
 }
